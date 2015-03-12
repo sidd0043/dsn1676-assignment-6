3451a630bd1b28ab0d8e3c15db90c7f46ee5cc0e
sidd0043 <Mukkadas@macbook-pro.gateway.2wire.net>
2015-03-11 20:50:21 -0400

    var btn-append

--- js/main.js
+++ js/main.js
@@ -6,4 +6,5 @@ var $btnCollapseExpand = $('.btn-collapse-expand');
 var $panel = $('.panel');
 var $btnBounce = $('.btn-bounce');
 var $circle = $('.circle');
-
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
